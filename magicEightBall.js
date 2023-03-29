3b1afe0215f4cdfaf1009b91d3db1b17e939be28
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-03-29 16:56:39 +0100

    Script for magic eight ball using switch statement

---
+++ magicEightBall.js
@@ -0,0 +1,41 @@
+let userName = '';
+
+userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+
+let userQuestion = 'What programming language should I learn?';
+
+console.log(`${userName}: ${userQuestion}`);
+
+// Generate a random number between 0 and 7
+const randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+    case 4:
+        eightBall = 'Do not count on it';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+}
+
+console.log(`Magic Eight Ball's answer (${randomNumber}): ${eightBall}`);
