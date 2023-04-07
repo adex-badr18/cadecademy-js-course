65894ac12eb5b7f72975694ba49bf3e304626e38
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-04-07 22:20:29 +0100

    Refactor: Change Book class to inherit Media class

--- libraryProject.js
+++ libraryProject.js
@@ -41,13 +41,11 @@ class Media {
     }
 }
 
-class Book {
+class Book extends Media {
     constructor(author, title, pages) {
+        super(title);
         this._author = author;
-        this._title = title;
         this._pages = pages;
-        this._isCheckedOut = false;
-        this._ratings = [];
     }
 
     // getter methods
@@ -55,42 +53,8 @@ class Book {
         return this._author;
     }
 
-    get title() {
-        return this._title;
-    }
-
     get pages() {
         return this._pages;
-    }
-
-    get ratings() {
-        return this._ratings;
-    }
-
-    get isCheckedOut() {
-        return this._isCheckedOut;
-    }
-
-    // instance methods
-    getAverageRating() {
-        const sum = this._ratings.reduce((accumulator, currentValue) => accumulator + currentValue)
-        const ratingsLength = this._ratings.length;
-        const averageRating = sum / ratingsLength;
-        return Math.round(averageRating * 10) / 10;
-    }
-
-    addRating(newRating) {
-        if (newRating > 5 || newRating < 1) {
-            console.log("Give a rating between 1 and 5.")
-        } else if (typeof newRating !== 'number') {
-            console.log('Numeric value expected');
-        }
-
-        this._ratings.push(newRating);
-    }
-
-    toggleCheckOutStatus() {
-        this._isCheckedOut = !this._isCheckedOut;
     }
 }
 
@@ -98,11 +62,11 @@ const newBook = new Book('John', 'New Book', 34);
 newBook.addRating(4);
 newBook.addRating(5);
 newBook.addRating(3);
-// console.log(newBook.getAverageRating());
+console.log(newBook.getAverageRating());
 
 newBook.toggleCheckOutStatus();
 newBook.toggleCheckOutStatus();
-// console.log(newBook._isCheckedOut);
+console.log(newBook._isCheckedOut);
 
 
 class Movie {
@@ -225,7 +189,7 @@ const newCD = new CD('John', 'New Movie', 60);
 newCD.addRating(5);
 newCD.addRating(5);
 newCD.addRating(5);
-console.log(newCD.getAverageRating());
+// console.log(newCD.getAverageRating());
 
 newCD.toggleCheckOutStatus();
-console.log(newCD._isCheckedOut);
+// console.log(newCD._isCheckedOut);
